09a7108e8bbefe60d4ec3da956ed1d721ece7277
Max Pavlov <maxpavlovdp@gmail.com>
2021-03-18 00:19:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# iaai-checker
---
+++ check.sh
@@ -0,0 +1,7 @@
+if curl -s "https://iaai.com/VehicleDetails?itemid=39259279" | grep "Auction not assigned"
+then
+    curl --location --request GET 'https://api.telegram.org/bot1722077150:AAFYL-X4YAxw4RgujHE6mmwsfS2CdWwLtbE/sendMessage?chat_id=-590015500&text=DesertHawkIsWaitingForMax' \
+--header 'Cookie: X-OpenDNS-Session=52c42a730918d044330bea30ae645a571fff9270fc58_DbfeuR1L' -k
+else
+    echo "Error"
+fi
