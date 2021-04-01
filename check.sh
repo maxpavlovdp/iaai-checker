a364044dd569c4aa5007b2c7abd2745b1dfcb89a
Max Pavlov <maxpavlovdp@gmail.com>
2021-04-01 15:46:45 +0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,9 +1,9 @@
 #if not curl -s "https://iaai.com/VehicleDetails?itemid=39259279" | grep "yyyyyyh"
-if curl -s "https://iaai.com/VehicleDetails?itemid=39259279" | grep "2,225"
+if curl -s "https://iaai.com/VehicleDetails?itemid=39259279" | grep "2,625"
 then
     date >> /git/iaai-checker/check.log
 else
-    curl --location --request GET 'https://api.telegram.org/bot1722077150:AAFYL-X4YAxw4RgujHE6mmwsfS2CdWwLtbE/sendMessage?chat_id=-590015500&text=your_bit_is_more_then_2225' \
+    curl --location --request GET 'https://api.telegram.org/bot1722077150:AAFYL-X4YAxw4RgujHE6mmwsfS2CdWwLtbE/sendMessage?chat_id=-590015500&text=your_bit_is_more_then_2625' \
     --header 'Cookie: X-OpenDNS-Session=52c42a730918d044330bea30ae645a571fff9270fc58_DbfeuR1L' -k
 fi
 
