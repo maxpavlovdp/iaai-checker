8ffa9f662055c613cee19fceb32f9055f0635ce8
Max Pavlov <maxpavlovdp@gmail.com>
2021-06-23 16:25:08 +0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,12 +1,12 @@
 #if not curl -s "https://iaai.com/VehicleDetails?itemid=39259279" | grep "yyyyyyh"
 # if curl -s "https://iaai.com/VehicleDetails?itemid=39259279" | grep "2,625"
-if curl -s "https://lion-trans.com/track_number?number=ZACCJBCB1HPE78193++" | grep "In Container"
-then
-    date >> /git/iaai-checker/check.log
-else
-    curl --location --request GET 'https://api.telegram.org/bot1722077150:AAFYL-X4YAxw4RgujHE6mmwsfS2CdWwLtbE/sendMessage?chat_id=-590015500&text=April-is-in-Odessa' \
-    --header 'Cookie: X-OpenDNS-Session=52c42a730918d044330bea30ae645a571fff9270fc58_DbfeuR1L' -k
-fi
+# if curl -s "https://lion-trans.com/track_number?number=ZACCJBCB1HPE78193++" | grep "In Container"
+# then
+#     date >> /git/iaai-checker/check.log
+# else
+#     curl --location --request GET 'https://api.telegram.org/bot1722077150:AAFYL-X4YAxw4RgujHE6mmwsfS2CdWwLtbE/sendMessage?chat_id=-590015500&text=April-is-in-Odessa' \
+#     --header 'Cookie: X-OpenDNS-Session=52c42a730918d044330bea30ae645a571fff9270fc58_DbfeuR1L' -k
+# fi
 
 cd /git/iaai-checker/
 git pull
