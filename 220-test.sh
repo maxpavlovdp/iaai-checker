47bfe5c0bdfa7826244743384e1a48aa89414986
Maks Pavlov <eskimorollerr@gmail.com>
2023-11-01 11:57:45 +0200

    checkout url security test

--- 220-test.sh
+++ 220-test.sh
@@ -4,7 +4,7 @@ git pull
 curl 'https://220-km-preprod.com:8080/api/device/v2/start?deviceFingerPrint=3a23fbd2-c321-4b47-89a4-0f77a154d2ca' \
   -H 'authority: 220-km.com:8080' \
   -H 'accept: application/json' \
-  -H 'accept-language: ru,en-US;q=0.9,en;q=0.8,uk;q=0.7' \
+  -H 'accept-language: ru,en-US;q=0.9,en;q=0.8,uk;q=0.7'
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
   -H 'origin: https://220-km.com' \
@@ -15,6 +15,20 @@ curl 'https://220-km-preprod.com:8080/api/device/v2/start?deviceFingerPrint=3a23
   -H 'sec-fetch-site: same-site' \
   -H 'user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1' \
   --data-raw '{"station_number":"2"}' \
+  --compressed
+
+curl 'https://220-km-preprod.com:8080/api/order/generateCheckoutLinkKwt?station_number=333&kwt=5&deviceFingerPrint=44d89705-dafb-402f-9750-a7fc4a305e2e' \
+  -H 'authority: 220-km-preprod.com:8080' \
+  -H 'accept: */*' \
+  -H 'accept-language: ru,en-US;q=0.9,en;q=0.8,uk;q=0.7' \
+  -H 'cache-control: no-cache' \
+  -H 'origin: https://220-km-preprod.com' \
+  -H 'pragma: no-cache' \
+  -H 'referer: https://220-km-preprod.com/' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: same-site' \
+  -H 'user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1' \
   --compressed
 
 
